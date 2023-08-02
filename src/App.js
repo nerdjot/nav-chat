2f9b6da617e85aafd61decc9ebc3861528e2d02f
Navjot Singh <navjot_@outlook.com>
2023-08-03 01:32:24 +0530

    added data rendering part of the chats app'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import Data from './Data'
+
+function changeTimeFormat(date) {
+  let n = date.toLocaleString([], {
+      hour: '2-digit',
+      minute: '2-digit'
+  });
+
+  return n
+}
+
+function ChatsItem({chatData})
+{
+  let timeObj = new Date(Number(chatData.messages[chatData.messages.length - 1].time));
+  return(
+    <div>
+      <div className ='row'>
+        <div className='col-8 text-left '>{chatData.name}</div>
+        <div className='col-4 text-right'>{changeTimeFormat(timeObj)}</div>
+      </div>
+      <div className ='row'>
+        <div className='col-8'>{chatData.messages[chatData.messages.length - 1].message}</div>
+        <div className='col-4'>unread</div>
+      </div>
+    </div>
+  );
+}
+
+function ChatsList({data})
+{
+  return(
+    data.map(dataItem => <ChatsItem id={dataItem.id} chatData={dataItem}></ChatsItem>)
+  );
+}
+
+function Chats({data})
+{
+  return (
+    <div className="col-4 h-100">
+      <div>Chats Navbar</div>
+      <ChatsList data={data}></ChatsList>
+    </div>
+  );
+}
+
+function ChatBubble({messageData})
+{
+  let myMessage = messageData.sender !== 'you';
+  return (
+    <div className={myMessage?"row justify-content-start":"row justify-content-end"}>
+      <div className={"col-5 rounded text-left "+ (myMessage?"justify-content-start bg-light":" text-light justify-content-end bg-primary")}>
+        {messageData.message}
+      </div>
+    </div>
+  );
+}
+
+function ChatBox({chatData})
+{
+  return (
+    <div className="col-6">
+      <div>{chatData.name}</div>
+      {chatData.messages.map(messageData => <ChatBubble messageData={messageData}></ChatBubble>)}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <div className="row">
+        <Chats data={Data}></Chats>
+        <ChatBox chatData={Data[0]}></ChatBox>
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,105 @@
+let Data = [
+  {
+    'id': '1',
+    'name': 'Swapnil More',
+    'image': 'url',
+    'messages': [
+      {
+        'sender':'you',
+        'time': '1690999511',
+        'message': 'Hey',
+        'read': 'read'
+      },
+      {
+        'sender':'them',
+        'time': '1690999520',
+        'message': 'Hey',
+        'read': 'read'
+      },
+      {
+        'sender':'you',
+        'time': '1690999600',
+        'message': 'Supports Unix timestamps in seconds, milliseconds, microseconds and nanoseconds.',
+        'read': 'read'
+      },
+      {
+        'sender':'them',
+        'time': '1690999711',
+        'message': 'Auto-layout for flexbox grid columns also means you can set the width of one column and have the sibling columns automatically resize around it. You may use predefined grid classes (as shown below), grid mixins, or inline widths. Note that the other columns will resize no matter the width of the center column.',
+        'read': 'read'
+      },
+      {
+        'sender':'you',
+        'time': '1691000511',
+        'message': 'Okay Bye',
+        'read': 'received'
+      },
+    ]
+  },
+  { 
+    'id': '2',
+    'name': 'Samantha Ruthprabhu',
+    'image': 'url',
+    'messages': [
+      {
+        'sender':'you',
+        'time': '1690999511',
+        'message': 'Hey'
+      },
+      {
+        'sender':'them',
+        'time': '1690999520',
+        'message': 'Hey'
+      },
+      {
+        'sender':'you',
+        'time': '1690999600',
+        'message': 'Supports Unix timestamps in seconds, milliseconds, microseconds and nanoseconds.'
+      },
+      {
+        'sender':'them',
+        'time': '1690999711',
+        'message': 'Auto-layout for flexbox grid columns also means you can set the width of one column and have the sibling columns automatically resize around it. You may use predefined grid classes (as shown below), grid mixins, or inline widths. Note that the other columns will resize no matter the width of the center column.'
+      },
+      {
+        'sender':'you',
+        'time': '1691000511',
+        'message': 'Okay Bye'
+      },
+    ]
+  },
+  {
+    'id': '3',
+    'name': 'Aalu Arjun',
+    'image': 'url',
+    'messages': [
+      {
+        'sender':'you',
+        'time': '1690999511',
+        'message': 'Hey'
+      },
+      {
+        'sender':'them',
+        'time': '1690999520',
+        'message': 'Hey'
+      },
+      {
+        'sender':'you',
+        'time': '1690999600',
+        'message': 'Supports Unix timestamps in seconds, milliseconds, microseconds and nanoseconds.'
+      },
+      {
+        'sender':'them',
+        'time': '1690999711',
+        'message': 'Auto-layout for flexbox grid columns also means you can set the width of one column and have the sibling columns automatically resize around it. You may use predefined grid classes (as shown below), grid mixins, or inline widths. Note that the other columns will resize no matter the width of the center column.'
+      },
+      {
+        'sender':'you',
+        'time': '1691000511',
+        'message': 'Okay Bye'
+      },
+    ]
+  }
+]
+
+export default Data;
