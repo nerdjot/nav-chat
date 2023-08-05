0089fef8499ce2738e9f5d5c13f0d16398730022
Navjot Singh <navjot_@outlook.com>
2023-08-05 17:09:38 +0530

    created rendering components for various features of the application

--- src/App.js
+++ src/App.js
@@ -1,78 +1,241 @@
 import logo from './logo.svg';
 import './App.css';
-import Data from './Data'
+import nav_data from './Data'
+import { FaCheckDouble } from "react-icons/fa";
 
-function changeTimeFormat(date) {
-  let n = date.toLocaleString([], {
-      hour: '2-digit',
-      minute: '2-digit'
-  });
+let myUserName = '1';
+let messages = nav_data.messages;
+let channels = nav_data.channels;
+let users = nav_data.users;
 
-  return n
+const isToday = (someDate) => {
+  const today = new Date()
+  return someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
+}
+const isYesterday = (someDate) => {
+  const today = new Date()
+  return someDate.getDate()+1 == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
 }
 
-function ChatsItem({chatData})
+function getTimeFormat(date)
 {
-  let timeObj = new Date(Number(chatData.messages[chatData.messages.length - 1].time));
+  return date.toLocaleString([], {
+    hour: '2-digit',
+    minute: '2-digit'});
+}
+
+function getDateFormat(date)
+{
+  return date.getDate() + "/" + (Number(date.getMonth())+1) + "/" + String(date.getFullYear()).slice(-2);
+}
+
+function getChatDateString(timestamp) {
+  let date = new Date(Number(timestamp)*1000);
+  if (isToday(date))
+  {
+    return "Today";
+  }
+  else if (isYesterday(date))
+  {
+    return "Yesterday";
+  }
+  else
+  {
+    return getDateFormat(date);
+  }
+}
+
+function getOtherUserName(members)
+{
+  if (members.length > 2)
+  {
+     return '';
+  }
+  let otherUserId = members.find((member) => member!==myUserName);
+  return users.find((user)=> user.id === otherUserId).name;
+}
+
+function getUser(id)
+{
+  return users.find((user)=>user.id === id);
+}
+
+function isMyMessage(message)
+{
+  return (message.sender === myUserName);
+}
+
+function isMessageReadByMe(message)
+{
+  return (isMyMessage(message)||(message.read_by.find((user) => user === myUserName)));
+}
+
+function getMessagesUnreadByMe(channel)
+{
+  let messagesUnreadByMe = 0;
+  let channelMessages = messages[channel.id];
+  let numberOfMessages = channelMessages.length;
+  for(let i=numberOfMessages; i>0; i--)
+  {
+    if (isMessageReadByMe(channelMessages[i-1]))
+    {
+      break;
+    }
+    else{
+      messagesUnreadByMe++;
+    }
+  }
+  return messagesUnreadByMe;
+}
+
+function isSentMessageReadByAll(channel)
+{
+  let channelMessages = messages[channel.id];
+  let numberOfMessages = channelMessages.length;
+  let lastMessage = getLastMessage(channel);
+  return lastMessage?(lastMessage.read_by.length === channel.members.length-1):false;
+}
+
+function UnreadMessages({messages, channel})
+{
+  let messagesUnreadByMe = getMessagesUnreadByMe(channel);
+  let noMessages = messages.length === 0;
+  
+  return (
+
+    noMessages?<></>:
+    messagesUnreadByMe?
+    <div style={{height:"25px", width:"25px"}} className='font-weight-bold rounded-circle bg-danger text-light'>{messagesUnreadByMe}</div>:
+    <div className={isSentMessageReadByAll(channel)?"text-primary":"text-secondary"}><FaCheckDouble/></div>
+  );
+}
+
+function getChannel(channelID)
+{
+  return channels.find((channel) => channelID === channel.id);
+}
+
+function getChannelName(channel)
+{
+  let isChannel = channel.name != 'na';
+  return isChannel?channel.name:getOtherUserName(channel.members);
+}
+
+function getLastMessage(channel)
+{
+  let channelMessages = messages[channel.id];
+  if (channelMessages.length === 0)
+  {
+    return null;
+  }
+  return channelMessages[channelMessages.length - 1];
+}
+
+function getLastMessageContent(channel)
+{
+  let lastMessage = getLastMessage(channel);
+  let isChannelGroup = channel.name != 'na';
+  return lastMessage?((isChannelGroup?getUser(lastMessage.sender).name+': ':'')+lastMessage.content) : '';
+}
+
+function getLastMessageTimeString(channel)
+{
+  let lastMessage = getLastMessage(channel);
+  if (lastMessage)
+  {
+    let date = new Date(Number(lastMessage.timestamp)*1000);
+    if (isToday(date))
+    {
+      return getTimeFormat(date);
+    }
+    else if (isYesterday(date))
+    {
+      return "Yesterday";
+    }
+    else
+    {
+      return getDateFormat(date);
+    }
+  }
+  return '';  
+}
+
+function ChannelsItem({channel, messages})
+{
   return(
-    <div>
-      <div className ='row'>
-        <div className='col-8 text-left '>{chatData.name}</div>
-        <div className='col-4 text-right'>{changeTimeFormat(timeObj)}</div>
+    <div className="bg-light border rounded">
+      <div className ='row justify-content-around'>
+        <div className='col-7 text-left '><h5>{getChannelName(channel)}</h5></div>
+        <div className='col-4 text-right'>{getLastMessageTimeString(channel)}</div>
       </div>
-      <div className ='row'>
-        <div className='col-8'>{chatData.messages[chatData.messages.length - 1].message}</div>
-        <div className='col-4'>unread</div>
+      <div className ='row justify-content-around'>
+        <div className='col-9'>{getLastMessageContent(channel)}</div>
+        <UnreadMessages channel={channel} messages={messages} myUserName={myUserName}></UnreadMessages>
       </div>
     </div>
   );
 }
 
-function ChatsList({data})
+function ChannelsList({channels, messages, users})
 {
   return(
-    data.map(dataItem => <ChatsItem id={dataItem.id} chatData={dataItem}></ChatsItem>)
+    channels.map(channel => <ChannelsItem key={channel.id} users={users} channel={channel} messages={messages[channel.id]}></ChannelsItem>)
   );
 }
 
-function Chats({data})
+function Channels({channels, messages, users})
 {
   return (
     <div className="col-4 h-100">
-      <div>Chats Navbar</div>
-      <ChatsList data={data}></ChatsList>
+      <div>Channels Navbar</div>
+      {
+      <ChannelsList channels={channels} messages={messages} users={users}></ChannelsList>
+      }
     </div>
   );
 }
 
-function ChatBubble({messageData})
+function ChatDateBubble({message})
 {
-  let myMessage = messageData.sender !== 'you';
   return (
-    <div className={myMessage?"row justify-content-start":"row justify-content-end"}>
-      <div className={"col-5 rounded text-left "+ (myMessage?"justify-content-start bg-light":" text-light justify-content-end bg-primary")}>
-        {messageData.message}
+    <div className="row col-12 justify-content-around"><div className='bg-light col-2 rounded text-secondary'>{getChatDateString(message.timestamp)}</div></div>
+  )
+}
+
+function ChatBubble({message})
+{
+  let myMessage = isMyMessage(message);
+  return (
+    <div className={!myMessage?"row justify-content-start":"row justify-content-end"}>
+      <div className={"col-5 rounded text-left "+ (!myMessage?"justify-content-start bg-light":" text-light justify-content-end bg-primary")}>
+        {message.content}
+        <div className={'text-right '+(!myMessage?'text-secondary':'')}>{getTimeFormat( new Date(Number(message.timestamp)*1000))}</div>
       </div>
     </div>
   );
 }
 
-function ChatBox({chatData})
+function ChatBox({messages, channel})
 {
   return (
-    <div className="col-6">
-      <div>{chatData.name}</div>
-      {chatData.messages.map(messageData => <ChatBubble messageData={messageData}></ChatBubble>)}
+    <div className="col-8">
+      <div><h3>{getChannelName(channel)}</h3></div>
+      {messages.map(message => <><ChatDateBubble message={message}></ChatDateBubble><ChatBubble message={message}></ChatBubble></>)}
     </div>
   );
 }
 
 function App() {
+  let currentChannelId = '1';
   return (
     <div className="App"> 
       <div className="row">
-        <Chats data={Data}></Chats>
-        <ChatBox chatData={Data[0]}></ChatBox>
+        <Channels channels={nav_data['channels']} messages={nav_data.messages} users={nav_data.users}></Channels>      
+        <ChatBox messages={messages[currentChannelId]} channel={getChannel(currentChannelId)}></ChatBox>
       </div>
     </div>
   );
--- src/Data.js
+++ src/Data.js
@@ -102,4 +102,181 @@ let Data = [
   }
 ]
 
-export default Data;
+let users = [
+  {
+    id: '1',
+    name: 'Navjot Singh',
+    email: 'navjot@gmail.com',
+    picture: 'abc.jpeg',
+    channels: ['1', '2', '3']
+  },
+  {
+    id: '2',
+    name: 'Swapnil More',
+    email: 'swapnil@gmail.com',
+    picture: 'abc.jpeg',
+    channels: ['1', '3']
+  },
+  {
+    id: '3',
+    name: 'Raavi',
+    email: 'raavi@gmail.com',
+    picture: 'abc.jpeg',
+    channels: ['2', '3']
+  }
+]
+
+let channels = [
+  {
+    id: '1',
+    name: 'na',
+    description: 'na',
+    creator_id: 'na',
+    members: ['1', '2']
+  },
+  {
+    id: '2',
+    name: 'na',
+    description: 'na',
+    creator_id: 'na',
+    members: ['1', '3']
+  },
+  {
+    id: '3',
+    name: 'Friends',
+    description: 'Friends description',
+    creator_id: '1',
+    members: ['1', '2', '3']
+  }
+]
+
+let messages = 
+  {
+    '1': //channel id
+    [
+      {
+        id: '1',
+        sender: '1',
+        content: 'Hey',
+        timestamp: '1690999511',
+        read_by:
+        [
+          '2'
+        ]
+      },
+      {
+        id: '2',
+        sender: '2',
+        content: 'Heyloo',
+        timestamp: '1691099511',
+        read_by:
+        [
+          '1'
+        ]
+      },
+      {
+        id: '3',
+        sender: '2',
+        content: 'Wassuppp',
+        timestamp: '1691399511',
+        read_by:
+        [
+          '1'
+        ]
+      },
+      {
+        id: '4',
+        sender: '1',
+        content: 'Nm man sup widya',
+        timestamp: '1691699511',
+        read_by:
+        [
+          '2'
+        ]
+      },
+      {
+        id: '5',
+        sender: '2',
+        content: 'Nm man sup widya NAVVVVV',
+        timestamp: '1691176455',
+        read_by:
+        [
+        ]
+      }
+    ],
+
+    '2':[
+      {
+        id: '11',
+        sender: '1',
+        content: 'Hey',
+        timestamp: '1690999511',
+        read_by:
+        [
+        ]
+      },
+    ],
+
+    '3': //channel id
+    [
+      {
+        id: '6',
+        sender: '1',
+        content: 'Hey',
+        timestamp: '1690999511',
+        read_by:
+        [
+          '2', '3'
+        ]
+      },
+      {
+        id: '7',
+        sender: '2',
+        content: 'Heyloo',
+        timestamp: '1691099511',
+        read_by:
+        [
+          '1', '3'
+        ]
+      },
+      {
+        id: '8',
+        sender: '3',
+        content: 'Wassuppp',
+        timestamp: '1691399511',
+        read_by:
+        [
+          '1', '2'
+        ]
+      },
+      {
+        id: '9',
+        sender: '2',
+        content: 'Nm man sup widya',
+        timestamp: '1691699511',
+        read_by:
+        [
+          '3'
+        ]
+      },
+      {
+        id: '10',
+        sender: '3',
+        content: 'Nm man sup widya',
+        timestamp: '1691091139',
+        read_by:
+        [
+          '2'
+        ]
+      }
+    ]
+  }
+
+
+let nav_data = {
+  'users':  users,
+  'channels': channels,
+  'messages': messages
+}
+
+export default nav_data;
